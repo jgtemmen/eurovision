a46688840cc263842219d0b8a047ab60f6cb1e5b
jgtemmen <35985384+jgtemmen@users.noreply.github.com>
2025-04-10 16:18:50 +0200

    Update eurovisiondashboard25.py

--- eurovisiondashboard25.py
+++ eurovisiondashboard25.py
@@ -89,9 +89,7 @@ fig = px.choropleth(EV25[EV25['country'] != 'Australia'],
                     fitbounds='locations',
                     hover_name='country',
                     title='Eurovision 2025 Participants')
-fig.update_geos(
-    zoom=4.5  # Increase the zoom level to 4.5
-)
+
 fig2 = px.choropleth(EV25[EV25['country'] == 'Australia'],
                     locations='country',
                     locationmode='country names',
